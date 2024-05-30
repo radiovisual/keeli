80e226d4e6366acbdcee24a7675ba633984dadcd
Michael Wuergler <wuergler@gmail.com>
2024-05-30 14:37:27 +0200

    chore: update dependencies

---
+++ src/config/default-config.ts
@@ -0,0 +1,13 @@
+const defaultConfig = {
+  defaultLocale: "en",
+  sourceFile: "en.json",
+  supportedTranslations: [],
+  pathToTranslatedFiles: "i18n",
+  rules: {
+    "no-untranslated-files": "error",
+  },
+  dryRun: false,
+  enabled: true,
+};
+
+export default defaultConfig;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import { Config } from "./types";
 
-const configPath = path.join(__dirname, "../config.json");
+const configPath = path.join(__dirname, "../i18n-validator.config.json");
 const config: Config = JSON.parse(fs.readFileSync(configPath, "utf8"));
 
 runRules(config);
